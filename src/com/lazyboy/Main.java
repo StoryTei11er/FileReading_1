7ec9f1b3fc67e224577888a9f94bacbe59926a77
StoryTei11er <sitnyanskij.aleksandr@gmail.com>
2020-06-09 16:14:56 +0300

    Пробовал сделать правильно.

--- src/com/lazyboy/Main.java
+++ src/com/lazyboy/Main.java
@@ -1,42 +1,53 @@
 package com.lazyboy;
 
 import java.io.*;
-import java.util.Scanner;
-
 
 public class Main {
 
     static String path = "./folder/folderChild/childData.txt";
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
 
-        File file = new File(path);
+        String target = readByBufferReader(path);
+        String[] stringNumbers = target.split(",");
+        int[] numbers = transformateToInt(stringNumbers);
+        parityCheck(numbers);
 
-        Scanner scanner = new Scanner(file);
-        String line = scanner.nextLine();
-        String[] numbersString = line.split(",");
-        int[] numbers = new int[11];
-        int counter = 0;
+    }
 
-        for (String number : numbersString) {
-            numbers[counter++] = Integer.parseInt(number);
+    public static String readByBufferReader(String path) {
+        String currentLine;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+            while ((currentLine = bufferedReader.readLine()) != null) {
+                return currentLine;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        throw new RuntimeException();
+    }
 
+    public static int[] transformateToInt(String[] array) {
+        int[] temp = new int[array.length];
+        int counter = 0;
+        for (String element : array)
+            temp[counter++] = Integer.parseInt(element);
+        return temp;
+    }
+
+    public static void parityCheck(int[] array) {
         int even = 0;
         int notEven = 0;
-        for (int qwe : numbers) {
-            if (qwe % 2 == 0) {
+
+        for (int number : array) {
+            if (number % 2 == 0) {
                 even++;
             } else {
                 notEven++;
             }
         }
-        System.out.println(even + "even");
-        System.out.println(notEven + "Not even");
-        scanner.close();
-
+        System.out.println(even + " Even" + "\n" + notEven + " Not even");
     }
-
 
 }
 
